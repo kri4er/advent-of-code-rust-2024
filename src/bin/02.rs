57ce7381c605d25de0bb19fc07e4e7b04e1687d4
kri4er <mynamekaldery@gmail.com>
2024-12-07 23:15:07 +0100

    day2: part 1

---
+++ src/bin/02.rs
@@ -0,0 +1,60 @@
+advent_of_code::solution!(2);
+
+fn is_decreasing(path: &Vec<i64>) -> bool {
+    for i in 0..path.len() - 1 {
+        if (path[i] > path[i+1]) && (path[i] - path[i + 1]) < 4 {
+            continue;
+        } else {
+            return false;
+        }
+    }
+    true
+}
+
+fn is_increasing(path: &Vec<i64>) -> bool {
+    for i in 0..path.len() - 1 {
+        if (path[i] < path[i+1]) && (path[i + 1] - path[i]) < 4 {
+            continue;
+        } else {
+            return false;
+        }
+    }
+    true
+}
+
+fn is_safe(path: &Vec<i64>) -> bool {
+    if path.len() < 2 { true } else {
+        is_decreasing(&path) || is_increasing(&path)
+    }
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let result:u32 = input.lines()
+        .map(|line|
+            line.split_whitespace()
+            .map(|num| num.parse::<i64>().unwrap()).collect::<Vec<i64>>())
+        .map(|parsed_slope| if is_safe(&parsed_slope) {1} else {0})
+        .sum();
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}

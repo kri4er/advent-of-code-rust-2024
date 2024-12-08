2674fd040ec72a57b7541ba46abcf728c6670647
kri4er <mynamekaldery@gmail.com>
2024-12-08 10:51:34 +0100

    day2: part 2 using improved

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,4 +1,6 @@
 advent_of_code::solution!(2);
+use itertools::Itertools;
+
 
 fn is_decreasing(path: &Vec<i64>) -> bool {
     for i in 0..path.len() - 1 {
@@ -39,7 +41,30 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let result:usize = input.lines()
+        .map(|line|
+            line.split_whitespace()
+            .map(|num| num.parse::<i64>().unwrap()).collect::<Vec<i64>>())
+        .filter(|nums| {
+            (0..nums.len()).any(|i| {
+                nums[0..i]
+                    .iter()
+                    .chain(&nums[i + 1..])
+                    .tuple_windows()
+                    .try_fold(0, |ord, (a, b)| {
+                        if ord >= 0 && (1..=3).contains(&(b - a)) {
+                            Ok(1)
+                        } else if ord <= 0 && (1..=3).contains(&(a - b)) {
+                            Ok(-1)
+                        } else {
+                            Err(())
+                        }
+                    })
+                .is_ok()
+            })
+        }
+        ).count();
+    Some(result as u32)
 }
 
 #[cfg(test)]
@@ -55,6 +80,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }

470e814d990f8ccd729e63194bc7f4697843177d
kri4er <mynamekaldery@gmail.com>
2025-01-01 12:31:39 +0100

    day 13 - part 1 brute

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -4,8 +4,17 @@ advent_of_code::solution!(12);
 
 use advent_of_code::util::union_find::UnionFind;
 
+#[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
+pub enum Side {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 #[derive(Debug, PartialEq, Hash, Eq, Clone, Copy)]
 pub struct Point(i32, i32);
+
 
 impl Point {
     pub fn add(self, other:&Point) -> Point {
@@ -17,6 +26,14 @@ impl Point {
 
     pub fn neighbours(self)->[Point;4] {
         ORTHOGONAL.map(|dir| self + dir)
+    }
+    pub fn neighbour_on_side(self, side:Side)->Point {
+        match side {
+            Side::Up => Point(self.0, self.1-1),
+            Side::Down => Point(self.0, self.1+1),
+            Side::Left => Point(self.0-1, self.1),
+            Side::Right => Point(self.0+1, self.1),
+        }
     }
 }
 
@@ -115,8 +132,107 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(result as u32)
 }
 
+fn sides(plots:&HashSet<Point>) -> u32 {
+        let mut side_set: HashSet<(Point, Side)> = HashSet::new();
+
+        for plot in plots {
+            for s in [Side::Up, Side::Down, Side::Left, Side::Right] {
+                if !plots.contains(&plot.neighbour_on_side(s)) {
+                    side_set.insert((*plot, s));
+                }
+            }
+        }
+
+        let mut grouped_sides: HashMap<(Point,Side), i32> = HashMap::new();
+        for side_entry in side_set.iter() {
+            let mut current = side_entry.0;
+
+            match side_entry.1 {
+                Side::Up|Side::Down => {
+                    let mut to_the_left = current.neighbour_on_side(Side::Left);
+                    while side_set.contains(&(to_the_left, side_entry.1)) {
+                        current = to_the_left;
+                        to_the_left = current.neighbour_on_side(Side::Left);
+                    }
+
+                    let key = (current, side_entry.1);
+                    if grouped_sides.contains_key(&key) {
+                        continue;
+                    }
+
+                    let mut side_length = 1;
+                    let mut to_the_right = current.neighbour_on_side(Side::Right);
+
+                    while side_set.contains(&(to_the_right, side_entry.1)) {
+                        current = to_the_right;
+                        to_the_right = current.neighbour_on_side(Side::Right);
+                        side_length += 1;
+                    }
+
+                    grouped_sides.insert(key, side_length);
+                }
+                Side::Left|Side::Right => {
+                    let mut below = current.neighbour_on_side(Side::Down);
+                    while side_set.contains(&(below, side_entry.1)) {
+                        current = below;
+                        below = current.neighbour_on_side(Side::Down);
+                    }
+
+                    let key = (current, side_entry.1);
+
+                    if grouped_sides.contains_key(&key) {
+                        continue;
+                    }
+
+                    let mut side_length = 1;
+                    let mut above = current.neighbour_on_side(Side::Up);
+                    while side_set.contains(&(above, side_entry.1)) {
+                        current = above;
+                        above = current.neighbour_on_side(Side::Up);
+                        side_length += 1;
+                    }
+
+                    grouped_sides.insert(key, side_length);
+                }
+            }
+        }
+
+        grouped_sides.len() as u32
+    }
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let m = input.lines()
+        .collect::<Vec<_>>();
+    let height = m.len();
+    let width = m[0].len();
+    let mut uf = (0..width*height).collect::<UnionFind<_>>();
+    let m:HashMap<Point, char> = vec_to_hashmap(&m);
+
+    for p in m.iter() {
+        let cur = p.0.clone();
+        for next in cur.neighbours() {
+            if m.get(&cur) == m.get(&next) {
+                let cur_id = cur.to_idx(width);
+                let next_id = next.to_idx(width);
+                uf.union(&cur_id, &next_id);
+            }
+        }
+    }
+    let mut regions:HashMap<usize, HashSet<Point>> = HashMap::new();
+
+    for p in m.iter() {
+        let cur = p.0.clone();
+        regions
+            .entry(uf.find(&cur.to_idx(width)).unwrap())
+            .or_default()
+            .insert(cur.clone());
+    }
+
+    let mut result = 0;
+    for region in regions.iter() {
+        result += region.1.len() * sides(region.1) as usize;
+    }
+    Some(result as u32)
 }
 
 #[cfg(test)]
@@ -132,6 +248,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(1206));
     }
 }
--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,7 +1,145 @@
+use std::{cmp::min, num::ParseIntError, ops::{Add, Mul, Sub}, str::FromStr};
+
+use itertools::Itertools;
+use nom::error::ParseError;
+
 advent_of_code::solution!(13);
 
+#[derive(Debug, PartialEq, Hash, Eq, Clone, Copy, PartialOrd, Ord)]
+pub struct Point(i32, i32);
+
+
+impl Point {
+    pub fn add(self, other:&Point) -> Point {
+        Point(self.0 + other.0, self.1 + other.1)
+    }
+    pub fn to_idx(self, width:usize) -> usize {
+        self.1 as usize * width + self.0 as usize
+    }
+}
+
+impl Sub for Point {
+    type Output = Point;
+
+    fn sub(self, other:Point) -> Point {
+        Point(self.0 - other.0, self.1 - other.1)
+    }
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other:Point) -> Point {
+        Point(self.0 + other.0, self.1 + other.1)
+    }
+}
+
+impl Mul for Point {
+    type Output = Point;
+
+    fn mul(self, other:Point) -> Point {
+        Point(self.0 * other.0, self.1 * other.1)
+    }
+
+}
+
+impl Mul<i32> for Point {
+    type Output = Point;
+
+    fn mul(self, other:i32) -> Point {
+        Point(self.0 * other, self.1 * other)
+    }
+
+}
+
+
+#[derive(Debug, PartialEq)]
+pub struct Game {
+    a: Point,
+    b: Point,
+    prize: Point,
+}
+
+impl Game {
+    fn parse_button(btn:&str) -> Result<Point, ParseIntError> {
+        let (_, coords) = btn.split_once(':').unwrap();
+        let (x, y) = coords.split_once(',').unwrap();
+
+        Ok(
+            Point(
+                x.trim().strip_prefix("X+").unwrap().parse()?,
+                y.trim().strip_prefix("Y+").unwrap().parse()?,
+            )
+        )
+    }
+    fn parse_prize(btn:&str) -> Result<Point, ParseIntError> {
+        let (_, coords) = btn.split_once(':').unwrap();
+        let (x, y) = coords.split_once(',').unwrap();
+
+        Ok(
+            Point(
+                x.trim().strip_prefix("X=").unwrap().parse()?,
+                y.trim().strip_prefix("Y=").unwrap().parse()?,
+            )
+        )
+    }
+
+    pub fn cost(p:&Point) -> i32 {
+        p.0*3 + p.1*1
+    }
+
+    pub fn solve_brute(&self) -> Option<Point> {
+        let mut best_value = i32::max_value();
+        let mut best_actions = Point(0, 0);
+        let press_a = Point(1, 0);
+        let press_b = Point(0, 1);
+
+        let mut cur_point = Point(0, 0);
+        while self.a * cur_point.0 < self.prize {
+            while self.b * cur_point.1 < self.prize {
+                let cur_point_value = (self.a * cur_point.0) + (self.b * cur_point.1);
+                if  cur_point_value == self.prize {
+                    if best_value > Game::cost(&cur_point_value) {
+                        best_value = Game::cost(&cur_point_value);
+                        best_actions = cur_point.clone();
+                    }
+                }
+                cur_point = cur_point + press_b;
+            }
+            //reset button b
+            cur_point = Point(cur_point.0, 0);
+            cur_point = cur_point + press_a;
+        }
+        Some(best_actions)
+    }
+
+}
+
+impl FromStr for Game {
+    type Err = std::num::ParseIntError;
+
+
+    fn from_str(data: &str) -> Result<Self, Self::Err> {
+        let mut it = data.splitn(3, '\n');
+
+        Ok(Game {
+            a: Game::parse_button(it.next().unwrap())?,
+            b: Game::parse_button(it.next().unwrap())?,
+            prize: Game::parse_prize(it.next().unwrap())?,
+        })
+    }
+
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let res:i32 = input
+        .split("\n\n")
+        .map(|game| game.parse::<Game>().unwrap())
+        .map(|game| game.solve_brute().unwrap())
+        .map(|p| Game::cost(&p))
+        .sum();
+
+    Some(res as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -15,7 +153,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(480));
     }
 
     #[test]
